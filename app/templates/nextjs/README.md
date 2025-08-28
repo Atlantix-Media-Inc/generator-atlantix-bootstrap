e62c1f48b57db5d5eda467a81773a98696061e19
andres-2dev <andres@atlantixmedia.ca>
2025-08-28 11:41:38 -0300

    feat: integrate nextjs generator

---
+++ app/templates/nextjs/README.md
@@ -0,0 +1,8 @@
+## New Atlantix Media Project
+This project was generated with [atlantix-bootstrap](https://www.npmjs.com/package/generator-atlantix-bootstrap).
+
+Happy coding 🌱
+
+### About
+This repository is maintained by everyone at [Atlantix Media Inc](https://atlantixmedia.ca/)
+![atlantix-media](./public/atalntix-brand.svg)
---
+++ app/templates/nextjs/page.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="flex items-center gap-4">
+          <Image
+            src="/atlantix-brand.svg"
+            alt="Atlantix logo"
+            width={280}
+            height={180}
+            priority
+          />
+          <h1 className="text-4xl font-bold">+</h1>
+          <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+        </div>
+        <div className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+          <p className="mb-2 tracking-[-.01em]">
+            Welcome to the Atlantix Next.js template!
+            <br />
+            You can start your magic editing this file:
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+              src/app/page.tsx
+            </code>
+            .
+          </p>
+          <p className="tracking-[-.01em]">
+            Learn more about our code standards and best practices
+          </p>
+        </div>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://github.com/Atlantix-Media-Inc/tech-guides"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
